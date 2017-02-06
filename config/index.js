0038e0137da1d1457387b860c30871dfcdce996e
Anders Opstrup <aopstrup@gmail.com>
2017-02-06 23:31:44 +0100

    added new connection string and dynamic port for service

--- config/index.js
+++ config/index.js
@@ -5,10 +5,10 @@ const config = {};
  */
 if (process.env.ENV == 'prod') {
     config.mongo = {
-        url: 'mongodb://fit360:tyTvzpBaESNRwi2UYUQQmihScxxOqNg9xrghxWX1gS1c218zWnyrtxxHxeUw819FxjSwXxF87sgp7HBBhRfPug==@fit360.documents.azure.com:10250/?ssl=true'
+        url: 'mongodb://api-fit360-mongodb:PE1cB425j8decmXTAF4XuQDFrf2J1bcry93qJamsm7kwYvr08T44JOm1vR3ReMqeI2Fb8gIwJh7zXRe5JvcaIg==@api-fit360-mongodb.documents.azure.com:10250/?ssl=true'
     };
     config.webservice = {
-        port: '80'
+        port: process.env.PORT
     };
 } else {
     config.mongo = {
